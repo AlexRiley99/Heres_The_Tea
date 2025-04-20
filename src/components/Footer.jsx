239bf9d3df77f50234ec4e6e41de6abb11015f3a
Alex Riley <alexriley1099@gmail.com>
2025-04-20 17:32:04 -0400

    Completed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,20 @@
 import '../styles/Footer.css';
+import tea_socials from '../assets/tea_socials.png';
 
 function Footer(){
     return(
     <footer>
-        <div className="hours">
-            <p>Hours:</p>
-            <p>6am - 5pm Daily</p>
-        </div>
-        <div className="locationPhoneContainer">
-            <div className="locationContainer">
-                <address className="location">217 Lavender Ln, Oakwood, OR 97621</address>
+        <div className="footerContents">
+            <div className="hoursContainer">
+                <p>Hours:</p>
+                <p>6am - 5pm Daily</p>
             </div>
-            <div className="phoneContainer">
+            <div className="locationAndPhone">
+                <address className="location">217 Lavender Ln, Oakwood, OR 97621</address>
                 <p>(980)-765-2134</p>
+            </div>
+            <div className="socialsContainer">
+                <img src={tea_socials} alt="Social media favicons" className="socials"></img>
             </div>
         </div>
     </footer>
